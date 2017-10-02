4337047aae4cbd4806c8112516a6779ad04c7370
Jeff Kilroy <jeffkilroy@gmail.com>
2017-10-02 16:46:53 -0400

    Parse composer lock to get version info

--- src/Support/Version/ApplicationVersionChecker.php
+++ src/Support/Version/ApplicationVersionChecker.php
@@ -7,24 +7,36 @@ class ApplicationVersionChecker
 
     public static function get()
     {
-        $tag = static::getGitTag();
-        $commitHash = static::getGitCommitHash();
-        $commitDate = static::getGitCommitDate();
-        $output = $tag.' ['.$commitHash.'] ('.$commitDate->format('Y-m-d H:m:s').')';
+        $composerLockJSON = static::getComposerLockJSON();
+        if(!$composerLockJSON){
+            return null;
+        }
+        $package = static::getPackageFromComposerLockJSON($composerLockJSON,'filepile/filepile-integration-laravel');
+        if(!$package){
+            return null;
+        }
+        $output = $package->version.' ['.$package->source->reference.']';
         return $output;
     }
 
-    private static function getGitTag(){
-        return trim(exec('git describe --tags --abbrev=0'));
+    private static function getComposerLockJSON(){
+        $filePath = base_path('composer.lock');
+        if(file_exists($filePath)){
+            $content = file_get_contents($filePath);
+            if(!empty($content)){
+                return json_decode($content);
+            }
+        }
+        return null;
     }
 
-    private static function getGitCommitHash(){
-        return trim(exec('git log --pretty="%h" -n1 HEAD'));
-    }
-
-    private static function getGitCommitDate(){
-        $commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
-        return $commitDate->setTimezone(new \DateTimeZone('UTC'));
+    private static function getPackageFromComposerLockJSON($composerLockJSON, $packageName){
+        foreach($composerLockJSON->packages as $package){
+            if($package->name == $packageName){
+                return $package;
+            }
+        }
+        return null;
     }
 
 }
